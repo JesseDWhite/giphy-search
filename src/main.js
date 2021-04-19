46989292af9880b82965ad84398cdbf5efef0f98
Jesse White <jesse.white6@gmail.com>
2021-04-19 16:31:27 -0700

    add initial files and api keys.
    co-authored-by: Rami Ahmed <ramiisinportland@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,5 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function () {
+    $('form#giphy').submit(function (event) {
+        event.preventDefault();
+        const search = $(".displayGiphy").val();
 
+        let request = new XMLHttpRequest();
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=9pAbu98pQ7GwDQtjPDXSvpNZ6ytaidRY&q=${search}&limit=25&offset=0&rating=g&lang=en${process.env.API_KEY}`;
+
+        request.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                const response = JSON.parse(this.responseText);
+                getElements(response);
+            }
+        };
+
+        request.open("GET", url, true);
+        request.send();
+
+
+        function getElements(response) {
+            $('.display').html(`${response.data[0].original}`);
+        }
+    });
 });
